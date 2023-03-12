299c63f2036105f97433e6172a081a2512e262d8
KitPyles <pyles.kit@gmail.com>
2023-03-12 14:55:28 -0500

    Completed Lesson 19

---
+++ src/main/java/guru/springframework/spring6webapp/domain/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring6webapp.domain.repositories;
+
+// created by kp on 3/12/2023
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<AuthorRepository, Long> {
+}
